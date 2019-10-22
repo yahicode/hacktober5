ca851b0f38bf53dab0d8288cda9b44218f04b0e0
iqra648 <56859860+iqra648@users.noreply.github.com>
2019-10-22 12:43:05 -0700

    Updated binary.c

--- binary.c
+++ binary.c
@@ -21,3 +21,27 @@ void main()
         temp=b;
   }
 }
+
+
+#include<stdio.h>
+#include<math.h>
+void main()
+{
+
+    int input,temp,n;
+    printf("Enter a number\n");
+    scanf("%d",&input);
+    temp=input;
+    n=temp;
+   while(temp>1)
+  { int count = 0;
+        do
+        {
+            temp=temp/2;
+            count++;
+        }while(temp>1);
+         n = n-pow(2,count);
+        printf("%d ",n);
+        temp=n;
+  }
+}

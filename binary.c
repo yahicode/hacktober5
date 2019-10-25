53adc4dc0d3dcb02d9f9226c86325fbb3a0ea1f5
anshusoni1803 <47084713+anshusoni1803@users.noreply.github.com>
2019-10-25 20:52:38 +0530

    binary.c

--- binary.c
+++ binary.c
@@ -21,3 +21,17 @@ void main()
         temp=b;
   }
 }
+// c program to find even-odd
+#include <stdio.h>
+int main()
+{
+    int number;
+    printf("Enter an integer: ");
+    scanf("%d", &number);
+    // True if the number is perfectly divisible by 2
+    if(number % 2 == 0)
+        printf("%d is even.", number);
+    else
+        printf("%d is odd.", number);
+    return 0;
+}
